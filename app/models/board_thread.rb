8ecba08b00f46b0160611419784a4c0593b91ed4
Alexey Fansky <afansky@gmail.com>
2012-08-25 23:00:57 +0300

    implementing last 5 posts disply

--- app/models/board_thread.rb
+++ app/models/board_thread.rb
@@ -1,4 +1,12 @@
 class BoardThread < ActiveRecord::Base
   belongs_to :board
-  has_many :posts
+  has_many :posts do
+    def most_recent
+      posts = (find :all, :order => "id desc", :limit => 5).reverse
+
+      first_post = find :first
+
+      posts.delete_if { |post| post == first_post }
+    end
+  end
 end
