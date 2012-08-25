709401806d6d9b2cf33afcdc3cc20c2eb4ca5a46
Alexey Fansky <afansky@gmail.com>
2012-08-25 23:50:34 +0300

    updating schema

--- db/migrate/20120711133946_initial_database.rb
+++ db/migrate/20120711133946_initial_database.rb
@@ -15,6 +15,7 @@ class InitialDatabase < ActiveRecord::Migration
       t.string :topic
       t.text :text
       t.date :creation_date
+      t.integer :post_number
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120711133946) do
     t.string  "topic"
     t.text    "text"
     t.date    "creation_date"
+    t.integer "post_number"
   end
 
 end
