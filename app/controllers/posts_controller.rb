c52b4ddee7c07f6041935089f6d6f389213031f9
Alexey Fansky <afansky@gmail.com>
2012-08-26 22:32:23 +0300

    implemented posts adding

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,11 @@ class PostsController < ApplicationController
   def create
     post = Post.new(params[:post])
 
+    post.creation_date = DateTime.current
+    post.post_number = Board.max_post_number(params[:board_id]) + 1
+
+    post.save
+
     redirect_to :back
   end
 end
